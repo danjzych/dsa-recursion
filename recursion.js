59d30b7d525850e2c5a4af5ef482a3046f8f0f42
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-11-07 12:31:41 -0800

    completed longest, wrote psuedo for everyOther

--- recursion.js
+++ recursion.js
@@ -9,22 +9,11 @@ function product(nums) {
 /** longest: return the length of the longest word in an array of words. */
 
 function longest(words) {
-  //if this word is longer, return this word
-  //if the other word is longer, return that one
+  if (words.length === 0) return 0;
 
-  //at the end, return the length of the word
-
-  // if (words.length === 1) return words[0].length;
-
-  // if (words[0].length <= words[1].length) {
-  //   return longest(words.slice(1));
-  // } else {
-  //   const resultArray = words.slice(2);
-  //   return longest(resultArray.push(words[0]));
-  // }
   const currentLength = words[0].length;
 
-  for (let el in words.slice(1)) {
+  for (let el of words.slice(1)) {
     if (currentLength < el.length) {
       return longest(words.slice(1));
     }
@@ -35,39 +24,45 @@ function longest(words) {
 
 /** everyOther: return a string with every other letter. */
 
-function everyOther(str) {}
+function everyOther(str) {
+  // "hello" -> h + unknown
+  // "llo" -> l + unknown
+  // "o" -> o
+  // hlo
+
+}
 
 /** find: return boolean depending on if val exists in array or not. */
 
-function find(arr, val) {}
+function find(arr, val) { }
 
 /** isPalindrome: checks whether a string is a palindrome or not. */
 
-function isPalindrome(str) {}
+function isPalindrome(str) { }
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str) { }
 
 /** findIndex: return the index of val in arr (or -1 if val is not present). */
 
-function findIndex(arr, val) {}
+function findIndex(arr, val) { }
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj) { }
 
 // FURTHER STUDY
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return true if val is in array, false if not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr, val) { }
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearchIndex(arr, val) {}
+function binarySearchIndex(arr, val) { }
 
 // you might find the above two problems easier if you change the function signature to:
 //
